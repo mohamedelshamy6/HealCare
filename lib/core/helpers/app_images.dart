81626b30dff158cc54fa0ffb8bad827fc2b5fddd
Mohamed Elshamy <73137782+mohamedelshamy6@users.noreply.github.com>
2024-12-05 13:11:47 +0200

    - Added all images and icons. - Added google signin package and ios configuration. - Added device preview for checking responsivity.

--- lib/core/helpers/app_images.dart
+++ lib/core/helpers/app_images.dart
@@ -1,6 +1,235 @@
 class Assets {
   Assets._();
 
+  /// Assets for iconsAgeIconBlue
+  /// assets/icons/age_icon_blue.svg
+  static const String iconsAgeIconBlue = "assets/icons/age_icon_blue.svg";
+
+  /// Assets for iconsBloodtypeIconBlue
+  /// assets/icons/bloodtype_icon_blue.svg
+  static const String iconsBloodtypeIconBlue =
+      "assets/icons/bloodtype_icon_blue.svg";
+
+  /// Assets for iconsBookingIconBlue
+  /// assets/icons/booking_icon_blue.svg
+  static const String iconsBookingIconBlue =
+      "assets/icons/booking_icon_blue.svg";
+
+  /// Assets for iconsBookingIconGrey
+  /// assets/icons/booking_icon_grey.svg
+  static const String iconsBookingIconGrey =
+      "assets/icons/booking_icon_grey.svg";
+
+  /// Assets for iconsCadiologist
+  /// assets/icons/cadiologist.png
+  static const String iconsCadiologist = "assets/icons/cadiologist.png";
+
+  /// Assets for iconsCalenderIconDarkblue
+  /// assets/icons/calender_icon_darkblue.svg
+  static const String iconsCalenderIconDarkblue =
+      "assets/icons/calender_icon_darkblue.svg";
+
+  /// Assets for iconsCallIconBlue
+  /// assets/icons/call_icon_blue.svg
+  static const String iconsCallIconBlue = "assets/icons/call_icon_blue.svg";
+
+  /// Assets for iconsChatIconBlue
+  /// assets/icons/chat_icon_blue.svg
+  static const String iconsChatIconBlue = "assets/icons/chat_icon_blue.svg";
+
+  /// Assets for iconsChatIconGrey
+  /// assets/icons/chat_icon_grey.svg
+  static const String iconsChatIconGrey = "assets/icons/chat_icon_grey.svg";
+
+  /// Assets for iconsClockIconDarkblue
+  /// assets/icons/clock_icon_darkblue.svg
+  static const String iconsClockIconDarkblue =
+      "assets/icons/clock_icon_darkblue.svg";
+
+  /// Assets for iconsClockSquareIconGrey
+  /// assets/icons/clock_square_icon_grey.png
+  static const String iconsClockSquareIconGrey =
+      "assets/icons/clock_square_icon_grey.png";
+
+  /// Assets for iconsDentists
+  /// assets/icons/dentists.png
+  static const String iconsDentists = "assets/icons/dentists.png";
+
+  /// Assets for iconsEWalletBlue
+  /// assets/icons/e_wallet_blue.svg
+  static const String iconsEWalletBlue = "assets/icons/e_wallet_blue.svg";
+
+  /// Assets for iconsEditIconBlue
+  /// assets/icons/edit_icon_blue.svg
+  static const String iconsEditIconBlue = "assets/icons/edit_icon_blue.svg";
+
+  /// Assets for iconsEditIconWhite
+  /// assets/icons/edit_icon_white.svg
+  static const String iconsEditIconWhite = "assets/icons/edit_icon_white.svg";
+
+  /// Assets for iconsEmailIconBlue
+  /// assets/icons/email_icon_blue.svg
+  static const String iconsEmailIconBlue = "assets/icons/email_icon_blue.svg";
+
+  /// Assets for iconsFavoriteIconDarkblueOutlined
+  /// assets/icons/favorite_icon_darkblue_outlined.png
+  static const String iconsFavoriteIconDarkblueOutlined =
+      "assets/icons/favorite_icon_darkblue_outlined.png";
+
+  /// Assets for iconsFavoriteIconRed
+  /// assets/icons/favorite_icon_red.png
+  static const String iconsFavoriteIconRed =
+      "assets/icons/favorite_icon_red.png";
+
+  /// Assets for iconsFavouriteIconRedOutlined
+  /// assets/icons/favourite_icon_red_outlined.png
+  static const String iconsFavouriteIconRedOutlined =
+      "assets/icons/favourite_icon_red_outlined.png";
+
+  /// Assets for iconsFilterIconWhite
+  /// assets/icons/filter_icon_white.svg
+  static const String iconsFilterIconWhite =
+      "assets/icons/filter_icon_white.svg";
+
+  /// Assets for iconsGastroenterologists
+  /// assets/icons/gastroenterologists.png
+  static const String iconsGastroenterologists =
+      "assets/icons/gastroenterologists.png";
+
+  /// Assets for iconsGenderIconBlue
+  /// assets/icons/gender_icon_blue.svg
+  static const String iconsGenderIconBlue = "assets/icons/gender_icon_blue.svg";
+
+  /// Assets for iconsHeartRate
+  /// assets/icons/heart_rate.png
+  static const String iconsHeartRate = "assets/icons/heart_rate.png";
+
+  /// Assets for iconsHeightIconBlue
+  /// assets/icons/height_icon_blue.svg
+  static const String iconsHeightIconBlue = "assets/icons/height_icon_blue.svg";
+
+  /// Assets for iconsHepatologists
+  /// assets/icons/hepatologists.png
+  static const String iconsHepatologists = "assets/icons/hepatologists.png";
+
+  /// Assets for iconsHomeIconBlue
+  /// assets/icons/home_icon_blue.svg
+  static const String iconsHomeIconBlue = "assets/icons/home_icon_blue.svg";
+
+  /// Assets for iconsHomeIconWhite
+  /// assets/icons/home_icon_white.svg
+  static const String iconsHomeIconWhite = "assets/icons/home_icon_white.svg";
+
+  /// Assets for iconsLocationIconBlue
+  /// assets/icons/location_icon_blue.svg
+  static const String iconsLocationIconBlue =
+      "assets/icons/location_icon_blue.svg";
+
+  /// Assets for iconsLocationIconDarkblue
+  /// assets/icons/location_icon_darkblue.svg
+  static const String iconsLocationIconDarkblue =
+      "assets/icons/location_icon_darkblue.svg";
+
+  /// Assets for iconsMicrophoneIconWhite
+  /// assets/icons/microphone_icon_white.svg
+  static const String iconsMicrophoneIconWhite =
+      "assets/icons/microphone_icon_white.svg";
+
+  /// Assets for iconsNephrologists
+  /// assets/icons/nephrologists.png
+  static const String iconsNephrologists = "assets/icons/nephrologists.png";
+
+  /// Assets for iconsNeurologists
+  /// assets/icons/neurologists.png
+  static const String iconsNeurologists = "assets/icons/neurologists.png";
+
+  /// Assets for iconsNotificationCalenderBlue
+  /// assets/icons/notification_calender_blue.svg
+  static const String iconsNotificationCalenderBlue =
+      "assets/icons/notification_calender_blue.svg";
+
+  /// Assets for iconsNotificationCalenderDarkblue
+  /// assets/icons/notification_calender_darkblue.svg
+  static const String iconsNotificationCalenderDarkblue =
+      "assets/icons/notification_calender_darkblue.svg";
+
+  /// Assets for iconsNotificationCalenderOrange
+  /// assets/icons/notification_calender_orange.svg
+  static const String iconsNotificationCalenderOrange =
+      "assets/icons/notification_calender_orange.svg";
+
+  /// Assets for iconsNotificationIconBlue
+  /// assets/icons/notification_icon_blue.svg
+  static const String iconsNotificationIconBlue =
+      "assets/icons/notification_icon_blue.svg";
+
+  /// Assets for iconsNotificationIconGrey
+  /// assets/icons/notification_icon_grey.svg
+  static const String iconsNotificationIconGrey =
+      "assets/icons/notification_icon_grey.svg";
+
+  /// Assets for iconsPayMoneyIconBlue
+  /// assets/icons/pay_money_icon_blue.svg
+  static const String iconsPayMoneyIconBlue =
+      "assets/icons/pay_money_icon_blue.svg";
+
+  /// Assets for iconsPlayVoiceIconWhite
+  /// assets/icons/play_voice_icon_white.svg
+  static const String iconsPlayVoiceIconWhite =
+      "assets/icons/play_voice_icon_white.svg";
+
+  /// Assets for iconsProfileIconBlue
+  /// assets/icons/profile_icon_blue.svg
+  static const String iconsProfileIconBlue =
+      "assets/icons/profile_icon_blue.svg";
+
+  /// Assets for iconsProfileIconGrey
+  /// assets/icons/profile_icon_grey.svg
+  static const String iconsProfileIconGrey =
+      "assets/icons/profile_icon_grey.svg";
+
+  /// Assets for iconsPsychiatrists
+  /// assets/icons/psychiatrists.png
+  static const String iconsPsychiatrists = "assets/icons/psychiatrists.png";
+
+  /// Assets for iconsPulmonologists
+  /// assets/icons/pulmonologists.png
+  static const String iconsPulmonologists = "assets/icons/pulmonologists.png";
+
+  /// Assets for iconsReviewIconWhite
+  /// assets/icons/review_icon_white.svg
+  static const String iconsReviewIconWhite =
+      "assets/icons/review_icon_white.svg";
+
+  /// Assets for iconsSearchIcon
+  /// assets/icons/search_icon.png
+  static const String iconsSearchIcon = "assets/icons/search_icon.png";
+
+  /// Assets for iconsSendFilesIconWhite
+  /// assets/icons/send_files_icon_white.svg
+  static const String iconsSendFilesIconWhite =
+      "assets/icons/send_files_icon_white.svg";
+
+  /// Assets for iconsStarIconYellow
+  /// assets/icons/star_icon_yellow.svg
+  static const String iconsStarIconYellow = "assets/icons/star_icon_yellow.svg";
+
+  /// Assets for iconsTrashIconRed
+  /// assets/icons/trash_icon_red.svg
+  static const String iconsTrashIconRed = "assets/icons/trash_icon_red.svg";
+
+  /// Assets for iconsVideoIconBlue
+  /// assets/icons/video_icon_blue.svg
+  static const String iconsVideoIconBlue = "assets/icons/video_icon_blue.svg";
+
+  /// Assets for iconsWalletIcon
+  /// assets/icons/wallet_icon.svg
+  static const String iconsWalletIcon = "assets/icons/wallet_icon.svg";
+
+  /// Assets for iconsWeightIconBlue
+  /// assets/icons/weight_icon_blue.svg
+  static const String iconsWeightIconBlue = "assets/icons/weight_icon_blue.svg";
+
   /// Assets for imagesAppIcon
   /// assets/images/app_icon.png
   static const String imagesAppIcon = "assets/images/app_icon.png";
@@ -9,6 +238,90 @@ class Assets {
   /// assets/images/choose_page_image.png
   static const String imagesChoosePageImage =
       "assets/images/choose_page_image.png";
+
+  /// Assets for imagesDoctorsDoctorF
+  /// assets/images/doctors/doctor_f.png
+  static const String imagesDoctorsDoctorF =
+      "assets/images/doctors/doctor_f.png";
+
+  /// Assets for imagesDoctorsDoctorF2
+  /// assets/images/doctors/doctor_f2.png
+  static const String imagesDoctorsDoctorF2 =
+      "assets/images/doctors/doctor_f2.png";
+
+  /// Assets for imagesDoctorsDoctorF3
+  /// assets/images/doctors/doctor_f3.png
+  static const String imagesDoctorsDoctorF3 =
+      "assets/images/doctors/doctor_f3.png";
+
+  /// Assets for imagesDoctorsDoctorM
+  /// assets/images/doctors/doctor_m.png
+  static const String imagesDoctorsDoctorM =
+      "assets/images/doctors/doctor_m.png";
+
+  /// Assets for imagesDoctorsDoctorM2
+  /// assets/images/doctors/doctor_m2.png
+  static const String imagesDoctorsDoctorM2 =
+      "assets/images/doctors/doctor_m2.png";
+
+  /// Assets for imagesDoctorsDoctorM3
+  /// assets/images/doctors/doctor_m3.png
+  static const String imagesDoctorsDoctorM3 =
+      "assets/images/doctors/doctor_m3.png";
+
+  /// Assets for imagesMasterCard
+  /// assets/images/master_card.png
+  static const String imagesMasterCard = "assets/images/master_card.png";
+
+  /// Assets for imagesPatientHomeBanner
+  /// assets/images/patient_home_banner.png
+  static const String imagesPatientHomeBanner =
+      "assets/images/patient_home_banner.png";
+
+  /// Assets for imagesPatientsPatientF
+  /// assets/images/patients/patient_f.png
+  static const String imagesPatientsPatientF =
+      "assets/images/patients/patient_f.png";
+
+  /// Assets for imagesPatientsPatientF2
+  /// assets/images/patients/patient_f2.png
+  static const String imagesPatientsPatientF2 =
+      "assets/images/patients/patient_f2.png";
+
+  /// Assets for imagesPatientsPatientF3
+  /// assets/images/patients/patient_f3.png
+  static const String imagesPatientsPatientF3 =
+      "assets/images/patients/patient_f3.png";
+
+  /// Assets for imagesPatientsPatientM
+  /// assets/images/patients/patient_m.png
+  static const String imagesPatientsPatientM =
+      "assets/images/patients/patient_m.png";
+
+  /// Assets for imagesPatientsPatientM2
+  /// assets/images/patients/patient_m2.png
+  static const String imagesPatientsPatientM2 =
+      "assets/images/patients/patient_m2.png";
+
+  /// Assets for imagesPatientsPatientM3
+  /// assets/images/patients/patient_m3.png
+  static const String imagesPatientsPatientM3 =
+      "assets/images/patients/patient_m3.png";
+
+  /// Assets for imagesPatientsPatientM4
+  /// assets/images/patients/patient_m4.png
+  static const String imagesPatientsPatientM4 =
+      "assets/images/patients/patient_m4.png";
+
+  /// Assets for imagesPatientsPatientM5
+  /// assets/images/patients/patient_m5.png
+  static const String imagesPatientsPatientM5 =
+      "assets/images/patients/patient_m5.png";
+
+  /// Assets for imagesPatientsPatientM6
+  /// assets/images/patients/patient_m6.png
+  static const String imagesPatientsPatientM6 =
+      "assets/images/patients/patient_m6.png";
 
   /// Assets for imagesSplashGradient
   /// assets/images/splash/gradient.png
@@ -25,6 +338,15 @@ class Assets {
   static const String imagesSplashSplashImage2 =
       "assets/images/splash/splash_image_2.png";
 
+  /// Assets for imagesVisa
+  /// assets/images/visa.png
+  static const String imagesVisa = "assets/images/visa.png";
+
+  /// Assets for svgsAppointmentCalender
+  /// assets/svgs/appointment_calender.svg
+  static const String svgsAppointmentCalender =
+      "assets/svgs/appointment_calender.svg";
+
   /// Assets for svgsChoosePageLine
   /// assets/svgs/choose_page_line.svg
   static const String svgsChoosePageLine = "assets/svgs/choose_page_line.svg";
@@ -32,4 +354,8 @@ class Assets {
   /// Assets for svgsGoogle
   /// assets/svgs/google.svg
   static const String svgsGoogle = "assets/svgs/google.svg";
+
+  /// Assets for svgsPaymentSuccess
+  /// assets/svgs/payment_success.svg
+  static const String svgsPaymentSuccess = "assets/svgs/payment_success.svg";
 }
