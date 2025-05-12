c661f5ebc77ac43a331921974f6e040baa204b2a
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-05-12 22:22:12 +0300

    - add AppointmentWithDoctorModel

--- lib/features/patient_booking/data/appointment_with_the_doctor_model.dart
+++ lib/features/patient_booking/data/appointment_with_the_doctor_model.dart
@@ -1,3 +1,13 @@
+import 'package:heal_care/features/auth/data/models/doctors_model.dart';
+import 'package:heal_care/features/patient_booking/data/appoientment_model.dart';
+
 class AppointmentWithDoctorModel {
+  final AppointmentModel appointment;
+  final DoctorsModel doctor;
+
+  AppointmentWithDoctorModel({
+    required this.appointment,
+    required this.doctor,
+  });
   
 }
