5043fe56ea27c4e3544635ca7cc7331a83084378
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-05-13 03:19:04 +0300

    - add DoctorBookingModel

---
+++ lib/features/doctor_booking/data/models/doctor_booking_model.dart
@@ -0,0 +1,40 @@
+class DoctorBookingModel {
+  String? id;
+  String? doctorId;
+  String? patientId;
+  String? appointmentDate;
+  String? appointmentTime;
+  String? status;
+  String? createdAt;
+
+  DoctorBookingModel(
+      {this.id,
+      this.doctorId,
+      this.patientId,
+      this.appointmentDate,
+      this.appointmentTime,
+      this.status,
+      this.createdAt});
+
+  DoctorBookingModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    doctorId = json['doctor_id'];
+    patientId = json['patient_id'];
+    appointmentDate = json['appointment_date'];
+    appointmentTime = json['appointment_time'];
+    status = json['status'];
+    createdAt = json['created_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['doctor_id'] = doctorId;
+    data['patient_id'] = patientId;
+    data['appointment_date'] = appointmentDate;
+    data['appointment_time'] = appointmentTime;
+    data['status'] = status;
+    data['created_at'] = createdAt;
+    return data;
+  }
+}
