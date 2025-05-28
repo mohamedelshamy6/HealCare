163092c7dfda27ebeee79a8d7100a2b9da647c8d
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-05-29 01:44:09 +0300

    finish  createConversation function

--- lib/features/chat/logic/cubit/chat_cubit.dart
+++ lib/features/chat/logic/cubit/chat_cubit.dart
@@ -1,8 +1,34 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:heal_care/core/helpers/app_constants.dart';
+import 'package:heal_care/features/auth/logic/cubit/doctors_cubit.dart';
+import 'package:heal_care/features/auth/logic/cubit/patients_cubit.dart';
+import 'package:heal_care/features/chat/data/repos/create_conversition_repository.dart';
 
 part 'chat_state.dart';
 
 class ChatCubit extends Cubit<ChatState> {
-  ChatCubit() : super(ChatInitial());
+  ChatCubit(this.conversitionRepository, this.doctorsCubit, this.patientsCubit)
+      : super(ChatInitial());
+
+  final CreateConversitionRepository conversitionRepository;
+
+  final DoctorsCubit doctorsCubit;
+  final PatientsCubit patientsCubit;
+
+  Future<void> createConversation() async {
+    emit(CreateChatConversitionLoading());
+    var result = await conversitionRepository.createConversition(
+      path: '${AppConstants.baseRestUrl}conversations',
+      body: {
+        'doctor_id': doctorsCubit.doctorsModel.first.id,
+        'patient_id': patientsCubit.patientsModel.first.id,
+      },
+    );
+    result.fold(
+      (error) => emit(CreateChatConversitionFailure(error)),
+      (_) {
+        emit(CreateChatConversitionSuccess());
+      },
+    );
+  }
 }
--- lib/features/chat/logic/cubit/chat_state.dart
+++ lib/features/chat/logic/cubit/chat_state.dart
@@ -1,17 +1,12 @@
 part of 'chat_cubit.dart';
 
-@immutable
 sealed class ChatState {}
 
 final class ChatInitial extends ChatState {}
 
 final class CreateChatConversitionLoading extends ChatState {}
 
-final class CreateChatConversitionSuccess extends ChatState {
-  final String message;
-
-  CreateChatConversitionSuccess(this.message);
-}
+final class CreateChatConversitionSuccess extends ChatState {}
 
 final class CreateChatConversitionFailure extends ChatState {
   final String error;
