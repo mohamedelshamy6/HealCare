89435dce31a13a642c88097ef8716dc88baf4bb9
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2025-06-10 19:40:09 +0300

    finish PatientFavouritesModel

---
+++ lib/features/auth/data/models/patient_favourotes_model.dart
@@ -0,0 +1,32 @@
+class PatientFavouritesModel {
+  String? doctorId;
+  String? doctorName;
+  String? doctorImage;
+  String? specialization;
+  double? averageRate;
+
+  PatientFavouritesModel(
+      {this.doctorId,
+      this.doctorName,
+      this.doctorImage,
+      this.specialization,
+      this.averageRate});
+
+  PatientFavouritesModel.fromJson(Map<String, dynamic> json) {
+    doctorId = json['doctor_id'];
+    doctorName = json['doctor_name'];
+    doctorImage = json['doctor_image'];
+    specialization = json['specialization'];
+    averageRate = json['average_rate'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['doctor_id'] = doctorId;
+    data['doctor_name'] = doctorName;
+    data['doctor_image'] = doctorImage;
+    data['specialization'] = specialization;
+    data['average_rate'] = averageRate;
+    return data;
+  }
+}
