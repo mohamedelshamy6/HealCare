768cc4f280079676706f6cd2b441c96f1269278a
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2025-06-10 19:45:25 +0300

    finish getPatientFavourites in  doctors cubit

--- lib/features/auth/logic/cubit/doctors_cubit.dart
+++ lib/features/auth/logic/cubit/doctors_cubit.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:heal_care/features/auth/data/models/patient_favourotes_model.dart';
+import 'package:heal_care/features/auth/data/repos/patient_favourites_repo.dart';
 
 import '../../../../core/helpers/app_constants.dart';
 import '../../data/models/doctors_model.dart';
@@ -7,10 +9,12 @@ import '../../data/repos/doctors_repo.dart';
 part 'doctors_state.dart';
 
 class DoctorsCubit extends Cubit<DoctorsState> {
-  DoctorsCubit({required this.doctorsRepo}) : super(DoctorsInitial());
+  DoctorsCubit({required this.doctorsRepo,required this.patientFavouritesRepo}) : super(DoctorsInitial());
 
   final DoctorsRepo doctorsRepo;
   List<DoctorsModel> doctorsModel = [];
+  List<PatientFavouritesModel> patientFavoritesModel = [];
+  final PatientFavouritesRepo patientFavouritesRepo;
   Future<void> getAllDoctors() async {
     emit(DoctorsLoading());
     final doctorsModel =
@@ -19,6 +23,23 @@ class DoctorsCubit extends Cubit<DoctorsState> {
       emit(DoctorsFailure(error: error));
     }, (doctorsModel) {
       this.doctorsModel = doctorsModel;
+      emit(DoctorsSuccess(doctorsModel: doctorsModel));
+    });
+  }
+
+  Future<void> getPatientFavourites(
+    String path,
+    dynamic data,
+  ) async {
+    emit(DoctorsLoading());
+    final response = await patientFavouritesRepo.getPatientFavourites(
+      path,
+      data,
+    );
+    response.fold((error) {
+      emit(DoctorsFailure(error: error));
+    }, (patientFavoritesModel) {
+      this.patientFavoritesModel = patientFavoritesModel;
       emit(DoctorsSuccess(doctorsModel: doctorsModel));
     });
   }
