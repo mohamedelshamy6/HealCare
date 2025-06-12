1d84dda980665c5e6536d5d78e25932731328f77
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2025-06-12 16:19:20 +0300

    finish getConversations

--- lib/features/chat/logic/cubit/chat_cubit.dart
+++ lib/features/chat/logic/cubit/chat_cubit.dart
@@ -3,11 +3,13 @@ import 'package:heal_care/core/helpers/app_constants.dart';
 import 'package:heal_care/features/auth/logic/cubit/doctors_cubit.dart';
 import 'package:heal_care/features/auth/logic/cubit/patients_cubit.dart';
 import 'package:heal_care/features/chat/data/repos/create_conversition_repository.dart';
+import 'package:heal_care/features/chat/data/repos/get_conversation_repo.dart';
 
 part 'chat_state.dart';
 
 class ChatCubit extends Cubit<ChatState> {
-  ChatCubit(this.conversitionRepository, this.doctorsCubit, this.patientsCubit)
+  ChatCubit(this.conversitionRepository, this.getConversationRepo,
+      this.doctorsCubit, this.patientsCubit)
       : super(ChatInitial());
 
   final CreateConversitionRepository conversitionRepository;
@@ -16,28 +18,48 @@ class ChatCubit extends Cubit<ChatState> {
   final PatientsCubit patientsCubit;
 
   Future<void> createConversation({
-  required String doctorId,
-  required String patientId,
-}) async {
-  if (isClosed) return;
+    required String doctorId,
+    required String patientId,
+  }) async {
+    if (isClosed) return;
 
-  emit(CreateChatConversitionLoading());
+    emit(CreateChatConversitionLoading());
 
-  final result = await conversitionRepository.createConversition(
-    path: '${AppConstants.baseRestUrl}conversations',
-    body: {
-      'doctor_id': doctorId,
-      'patient_id': patientId,
-    },
-  );
+    final result = await conversitionRepository.createConversition(
+      path: '${AppConstants.baseRestUrl}conversations',
+      body: {
+        'doctor_id': doctorId,
+        'patient_id': patientId,
+      },
+    );
 
-  if (isClosed) return;
+    if (isClosed) return;
 
-  result.fold(
-    (error) => emit(CreateChatConversitionFailure(error)),
-    (_) => emit(CreateChatConversitionSuccess()),
-  );
-}
+    result.fold(
+      (error) => emit(CreateChatConversitionFailure(error)),
+      (_) => emit(CreateChatConversitionSuccess()),
+    );
+  }
 
+  final GetConversationRepo getConversationRepo;
 
+  Future<void> getConversations({
+    required String userId,
+    required String userType,
+  }) async {
+    emit(GetChatConversitionLoading());
+
+    final result = await getConversationRepo.getConversations(
+      '${AppConstants.baseRestUrl}conversations',
+      {
+        'user_id': userId,
+        'user_type': userType,
+      },
+    );
+
+    result.fold(
+      (error) => emit(GetChatConversitionFailure(error)),
+      (_) => emit(GetChatConversitionSuccess()),
+    );
+  }
 }
