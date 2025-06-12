b5c22a2a6d358a5204bef30fb82de656f107ac07
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2025-06-12 16:14:03 +0300

    finish getConversations

---
+++ lib/features/chat/data/repos/get_conversation_repo.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:heal_care/core/errors/api/exceptions/api_exception.dart';
+import 'package:heal_care/core/networking/api_services.dart';
+import 'package:heal_care/features/chat/data/models/get_conversations_model.dart';
+
+class  GetConversationRepo {
+  final ApiServices apiServices;
+
+  GetConversationRepo({required this.apiServices});
+
+  Future<Either<String,List<GetConversationsModel>>> getConversations(
+    String path,Map<String,dynamic> data
+  ) async {
+    try {
+      final response = await apiServices.get(path, data: data);
+      return Right(response);
+    } on ApiException catch (e) {
+      return Left(e.errorModel.message.toString());
+    } 
+  }
+}
