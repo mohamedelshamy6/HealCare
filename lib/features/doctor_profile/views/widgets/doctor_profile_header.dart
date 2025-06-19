b7320aa3b8a2bac006ef4bd07a1c415a85683d8d
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2025-06-20 00:48:40 +0300

    finish fixing bugs

--- lib/features/doctor_profile/views/widgets/doctor_profile_header.dart
+++ lib/features/doctor_profile/views/widgets/doctor_profile_header.dart
@@ -1,9 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../core/helpers/spacing.dart';
 import '../../../../core/theme/app_colors.dart';
-
-import '../../../../core/helpers/app_images.dart';
 import '../../../../core/theme/app_text_styles.dart';
 
 class DoctorProfileHeader extends StatelessWidget {
@@ -30,12 +29,22 @@ class DoctorProfileHeader extends StatelessWidget {
               height: 80.h,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(32.r),
-                image: DecorationImage(
-                  image: image.isNotEmpty
-                      ? NetworkImage(image) as ImageProvider
-                      : const AssetImage(Assets.imagesDoctorsDoctorM2),
-                  fit: BoxFit.fill,
+              ),
+              child: CachedNetworkImage(
+                imageUrl: image,
+                imageBuilder: (context, imageProvider) => Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(32.r),
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
                 ),
+                placeholder: (context, url) => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+                errorWidget: (context, url, error) => const Icon(Icons.person),
               ),
             ),
             CircleAvatar(
